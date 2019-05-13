e26f01efe134d46946d2cd9ba19bb948f41f0f83
kodigogj <50257248+kodigogj@users.noreply.github.com>
2019-05-13 16:35:04 +0800

    Add files via upload

--- kod_array.h
+++ kod_array.h
@@ -1,51 +1,37 @@
 #pragma once
 #include <assert.h>
-#include <cstring>
 #include <new>
 #include <type_traits>
 
 namespace kod {
 template <typename T>
-struct array {
-private:
-    struct allocator {
-        template <bool /* unsafe */>
-        inline constexpr static void copy(T* const& ptr, const T& value) { new (ptr) T{ value }; }
-        template <>
-        inline constexpr static void copy<true>(T* const& ptr, const T& value) { memcpy(ptr, &value, sizeof(T)); }
-
-        template <bool /* unsafe */>
-        inline constexpr static void move(T* const& ptr, const T& value) { new (ptr) T{ std::move(value) }; }
-        template <>
-        inline constexpr static void move<true>(T* const& ptr, const T& value) { memcpy(ptr, &value, sizeof(T)); }
-
-        template <bool = std::disjunction_v<std::has_virtual_destructor<T>, std::conjunction<std::is_destructible<T>, std::negation<std::is_trivially_destructible<T>>>>>
-        inline constexpr static void free(T* const& data, const size_t& start, const size_t& end)
-        {
-
-            if (data) {
-                for (T* i = data + start; i <= data + end; ++i) {
-                    i->~T();
-                }
+struct array_destructor {
+    template <bool = std::disjunction_v<std::has_virtual_destructor<T>, std::conjunction<std::is_destructible<T>, std::negation<std::is_trivially_destructible<T>>>>>
+    inline constexpr static void destroy(T* const& data)
+    {
+        if (data)
+            data->~T();
+    }
+    template <>
+    inline constexpr static void destroy<false>(T* const& data) {}
+    template <bool = std::disjunction_v<std::has_virtual_destructor<T>, std::conjunction<std::is_destructible<T>, std::negation<std::is_trivially_destructible<T>>>>>
+    inline constexpr static void destroy(T* const& data, const size_t& start, const size_t& end)
+    {
+        if (data) {
+            for (T* i = data + start; i <= data + end; ++i) {
+                i->~T();
             }
         }
-        template <>
-        inline constexpr static void free<false>(T* const& data, const size_t& start, const size_t& end) {}
-
-        template <bool = std::disjunction_v<std::has_virtual_destructor<T>, std::conjunction<std::is_destructible<T>, std::negation<std::is_trivially_destructible<T>>>>>
-        inline constexpr static void free(T* const& data)
-        {
-            if (data)
-                data->~T();
-        }
-        template <>
-        inline constexpr static void free<false>(T* const& data) {}
-    };
-
+    }
+    template <>
+    inline constexpr static void destroy<false>(T* const& data, const size_t& start, const size_t& end) {}
+};
+template <typename T, typename Destructor = array_destructor<T>>
+struct array {
+private:
     T* mdata{ nullptr };
-    //size_t* mcount{ nullptr }; //[3]{size, capacity, ref}
     size_t msize{ 0 }, mcapacity{ 0 };
-
+    //-----
 public:
     inline array() {}
     inline array(size_t capacity)
@@ -59,8 +45,7 @@ public:
         , mcapacity{ arg.mcapacity }
     {
     }
-
-public:
+    //-----
     inline constexpr size_t size() const { return msize; }
     inline constexpr size_t capacity() const { return mcapacity; }
     inline constexpr T& front() const
@@ -73,9 +58,8 @@ public:
         assert(msize > 1);
         return mdata[msize - 1];
     }
-
-public:
-    template <typename... Args>
+    //-----
+    template <typename... Args, std::enable_if_t<std::is_same_v<decltype(T{ Args{}... }), T>, int> = 0>
     inline constexpr bool emplace(size_t i, Args&&... args)
     {
         /*if (i > msize) {
@@ -95,11 +79,15 @@ public:
                 return false;
             }
         }
-        new (mdata + i) T(args...);
+        ::new (mdata + i) T{ args... };
         return true;
     }
-
-    template <typename... Args>
+    template <std::enable_if_t<std::is_copy_constructible_v<T>, int> = 0>
+    inline constexpr bool push(size_t i, const T& arg)
+    {
+        return emplace(i, arg);
+    }
+    template <typename... Args, std::enable_if_t<std::is_same_v<decltype(T{ Args{}... }), T>, int> = 0>
     inline constexpr bool emplace_back(Args&&... args)
     {
         if (msize == mcapacity) {
@@ -110,98 +98,20 @@ public:
                 return false;
             }
         }
-        new (mdata + ((++msize) - 1)) T(args...);
+        ::new (mdata + ((++msize) - 1)) T{ args... };
         return true;
     }
-
-    template <bool unsafe = std::enable_if_t<std::is_copy_constructible_v<T>, std::is_trivially_copy_constructible<T>>::value>
-    inline constexpr bool push(size_t i, const T& arg)
-    {
-        /*if (i > msize) {
-            return false;
-        }*/
-
-        assert(i <= msize);
-
-        if (msize == mcapacity) {
-            if (void* data = realloc(mdata, sizeof(T) * (++mcapacity))) {
-                mdata = static_cast<T*>(data);
-                if (i != (++msize) - 1) {
-                    memcpy(mdata + (i + 1), mdata + i, sizeof(T) * (msize - i - 1));
-                }
-            } else {
-                --mcapacity;
-                return false;
-            }
-        }
-        //new (mdata + i) T{ arg };
-        allocator::copy<unsafe>(mdata + i, arg);
-        return true;
-    }
-
-    template <bool unsafe = std::enable_if_t<std::is_move_constructible_v<T>, std::is_trivially_move_constructible<T>>::value>
-    inline constexpr bool push(size_t i, T&& arg)
-    {
-        /*if (i > msize) {
-            return false;
-        }*/
-
-        assert(i <= msize);
-
-        if (msize == mcapacity) {
-            if (void* data = realloc(mdata, sizeof(T) * (++mcapacity))) {
-                mdata = static_cast<T*>(data);
-                if (i != (++msize) - 1) {
-                    memcpy(mdata + (i + 1), mdata + i, sizeof(T) * (msize - i - 1));
-                }
-            } else {
-                --mcapacity;
-                return false;
-            }
-        }
-        //new (mdata + i) T{ std::move(arg) };
-        allocator::move<unsafe>(mdata + i, arg);
-        return true;
-    }
-
-    template <bool unsafe = std::enable_if_t<std::is_copy_constructible_v<T>, std::is_trivially_copy_constructible<T>>::value>
+    template <std::enable_if_t<std::is_copy_constructible_v<T>, int> = 0>
     inline constexpr bool push_back(const T& arg)
     {
-        if (msize == mcapacity) {
-            if (void* data = realloc(mdata, sizeof(T) * (++mcapacity))) {
-                mdata = static_cast<T*>(data);
-            } else {
-                --mcapacity;
-                return false;
-            }
-        }
-        //new (mdata + ((++msize) - 1)) T{ arg };
-        allocator::copy<unsafe>(mdata + ((++msize) - 1), arg);
-        return true;
+        return emplace_back(arg);
     }
-
-    template <bool unsafe = std::enable_if_t<std::is_move_constructible_v<T>, std::is_trivially_move_constructible<T>>::value>
-    inline constexpr bool push_back(T&& arg)
-    {
-        if (msize == mcapacity) {
-            if (void* data = realloc(mdata, sizeof(T) * (++mcapacity))) {
-                mdata = static_cast<T*>(data);
-            } else {
-                --mcapacity;
-                return false;
-            }
-        }
-        //new (mdata + ((++msize) - 1)) T{ std::move(arg) };
-        allocator::move<unsafe>(mdata + ((++msize) - 1), arg);
-        return true;
-    }
-
+    //-----
     inline constexpr T&& pop_back()
     {
         assert(msize > 1);
         return std::move(mdata[--msize]);
     }
-
     template <bool identity = true>
     inline constexpr size_t find(const T& arg)
     {
@@ -212,7 +122,6 @@ public:
         }
         return -1;
     }
-
     template <>
     inline constexpr size_t find<false>(const T& arg)
     {
@@ -223,14 +132,13 @@ public:
         }
         return -1;
     }
-
     template <bool sorted = true>
     inline constexpr bool remove(const T& arg)
     {
         for (size_t i = 0; i < msize; i++) {
             T* ptr = mdata + i;
             if (ptr == &arg) {
-                allocator::free(ptr);
+                Destructor::destroy(ptr);
                 if (i != --msize) {
                     memcpy(mdata + i, mdata + (i + 1), sizeof(T) * (msize - i));
                 }
@@ -239,14 +147,13 @@ public:
         }
         return false;
     }
-
     template <>
     inline constexpr bool remove<false>(const T& arg)
     {
         for (size_t i = 0; i < msize; i++) {
             T* ptr = mdata + i;
             if (ptr == &arg) {
-                allocator::free(ptr);
+                Destructor::destroy(ptr);
                 if (i != --msize) {
                     memcpy(mdata + i, mdata + msize, sizeof(T));
                 }
@@ -260,7 +167,7 @@ public:
     inline constexpr bool removeAt(size_t i)
     {
         if (i < msize) {
-            allocator::free(mdata[i]);
+            Destructor::destroy(mdata[i]);
             if (i != --msize) {
                 memcpy(mdata + i, mdata + (i + 1), sizeof(T) * (msize - i));
             }
@@ -274,7 +181,7 @@ public:
     inline constexpr bool removeAt<false>(size_t i)
     {
         if (i < msize) {
-            allocator::free(mdata[i]);
+            Destructor::destroy(mdata[i]);
             if (i != --msize) {
                 //new (mdata + i) T{ std::move(mdata[msize]) };
                 memcpy(mdata + i, mdata + msize, sizeof(T));
@@ -295,7 +202,7 @@ public:
 
         //if (i < msize && j < msize) { // TODO ASSERT?
         if (i < msize) {
-            allocator::free(mdata, i, j);
+            Destructor::destroy(mdata, i, j);
             if (i != (msize -= ((j - i) + 1))) {
                 memcpy(mdata + i, mdata + (j + 1), sizeof(T) * (msize - i));
             }
@@ -322,7 +229,7 @@ public:
 
     inline constexpr void clear()
     {
-        allocator::free(mdata, 0, msize - 1);
+        Destructor::destroy(mdata, 0, msize - 1);
         msize = 0;
     }
 
@@ -344,7 +251,7 @@ public:
 
     inline constexpr void reset()
     {
-        allocator::free(mdata, 0, msize - 1);
+        Destructor::destroy(mdata, 0, msize - 1);
         free(mdata);
         mdata = nullptr;
         msize = mcapacity = 0;
@@ -360,10 +267,10 @@ public:
     }
     inline constexpr bool operator==(const array& arg) const { return mdata == arg.mdata; }
     inline constexpr bool operator!=(const array& arg) const { return mdata != arg.mdata; }
-
-    inline ~array()
+    //-----
+    ~array()
     {
-        allocator::free(mdata, 0, msize - 1);
+        Destructor::destroy(mdata, 0, msize - 1);
         free(mdata);
     }
 };
